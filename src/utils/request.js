c3182f636c53df404dd58b5b3ee7ca667358e29b
tusdasa <tusdasa@tusdasa.net>
2020-07-23 16:17:26 +0800

    修改为http协议

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: 'https://api.tusdasa.net/api/',
+  baseURL: 'http://api.tusdasa.net/api/',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 50000 // request timeout
 })
